de5b5bc4e876a912b74fbb0882cb09c885e8798c
Nisha <rainishani400@gmail.com>
2025-01-02 08:55:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//let n=6;
+ //for(let i=0;i<n;i++){
+   // console.log("hello beautiful people" ,i);
+ //}
+ //console.log("bye see you soon");
+ //const nisha = require("./math");
+ //console.log(nisha.sum(2,2));
+ //console.log(nisha.mul(6,8));
+ //console.log(nisha.PI);
+ const info = require("./Fruits");
+ console.log(info);
