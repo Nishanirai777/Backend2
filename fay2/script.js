bf8fced5124f7a3baa90837eddac05cebbd1ca62
Nisha <rainishani400@gmail.com>
2025-01-03 15:52:03 +0545

    Add files via upload

---
+++ fay2/script.js
@@ -0,0 +1,15 @@
+//let n=6;
+ //for(let i=0;i<n;i++){
+   // console.log("hello beautiful people" ,i);
+ //}
+ //console.log("bye see you soon");
+ //const nisha = require("./math");
+ //console.log(nisha.sum(2,2));
+ //console.log(nisha.mul(6,8));
+ //console.log(nisha.PI);
+ //const info = require("./Fruits");
+ //console.log(info);
+ import { sum , PI } from "./math.js";
+ import { generate } from "random-words";
+ //console.log(sum(3,4));
+ console.log(generate());
