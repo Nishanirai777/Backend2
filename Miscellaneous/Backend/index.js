0fb8482482d65b1162869802a074cd2379c075ec
Nisha <rainishani400@gmail.com>
2025-01-10 06:19:24 +0545

    Update index.js

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 6060;
-app.get("/register",(req, res)=>{
-    res.send("standard GET response");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+
+    res.send(`standard GET response.WElcome ${user}!`);
 });
-app.post("/register",(req, res)=>{
-res.send("Standard POST response");
+app.post("/register", (req, res) => {
+    let {user, password}= req.body;
+    res.send(`Standard POST response.Welcome ${user}!`);
 });
 
 app.listen(port, () => {
-console.log(`listening to port${port}`);
+    console.log(`listening to port${port}`);
 });
